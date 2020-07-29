782cbb1df0895a8da945eb5a8816ad5fe59ab735
charlespimentel <pimentelufrj@gmail.com>
2020-07-29 14:11:25 +0000

    Atualiza o Main

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@
 # alpha.main.py
 # SPDX-License-Identifier: GPL-3.0-or-later
 """ Tutorial Dois - Brincando de git
-.. codeauthor:: Charles Soares Pimentel ,pimentelufrj@gmail.com>
-. Como criar um repositório no Github
-. Como clonar usando o comando git
-. Como comitar usando o comando git
+
+.. codeauthor:: Charles Soares Pimentel <pimentelufrj@gmail.com>
+
+- Como criar um repositório no Github
+- Como clonar usando o comando git
+- Como comitar usando o comando git
 
 Classes neste módulo:
     :py:class:'Main' Exemplo de classe qualquer
@@ -14,11 +16,13 @@ Changelog
 ---------------
 .. versionadded::     20.07
           Documentação do tutorial.
-   """
+"""
 
 class Main:
    """Classe exemplo.
+
         :param versao: versao deste exemplo.
+
    """
    def __init__(self, versao="20.07"):
         self.versao = versao
@@ -26,7 +30,8 @@ class Main:
 
    def get_versao(self):
         """Retorna a versao do sistema""
-            :return: A versao do sistema.
+
+           :return: A versao do sistema.
         """
         return self.versao
 
